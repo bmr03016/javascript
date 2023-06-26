c39ddd3ba1a4082ae056613a8521abb146ee24a5
bmr03016 <bmr03016@naver.com>
2023-06-26 21:13:52 +0900

    계산기 뼈대

---
+++ script/calculator.js
@@ -0,0 +1,23 @@
+let numOne = "";
+let opertaor = "";
+let numTwo = "";
+
+const $operator = document.querySelector("#operator");
+const $result = document.querySelector("#result");
+
+document.querySelector("#num-0").addEventListener("click", () => {}); // id는 #인거 안까먹었지?
+document.querySelector("#num-1").addEventListener("click", () => {});
+document.querySelector("#num-2").addEventListener("click", () => {});
+document.querySelector("#num-3").addEventListener("click", () => {});
+document.querySelector("#num-4").addEventListener("click", () => {});
+document.querySelector("#num-5").addEventListener("click", () => {});
+document.querySelector("#num-6").addEventListener("click", () => {});
+document.querySelector("#num-7").addEventListener("click", () => {});
+document.querySelector("#num-8").addEventListener("click", () => {});
+document.querySelector("#num-9").addEventListener("click", () => {});
+document.querySelector("#plus").addEventListener("click", () => {});
+document.querySelector("#minus").addEventListener("click", () => {});
+document.querySelector("#divide").addEventListener("click", () => {});
+document.querySelector("#multiply").addEventListener("click", () => {});
+document.querySelector("#calculate").addEventListener("click", () => {});
+document.querySelector("#clear").addEventListener("click", () => {});
